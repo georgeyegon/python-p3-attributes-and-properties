dc70554dba7771f1d816d68c1e98aa71990e58a0
George Yegon <georgeyegon67@gmail.com>
2024-12-04 10:39:52 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = ['Labrador', 'Poodle', 'Bulldog']  # Example breeds
+
+    def __init__(self, name, breed=None):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self.name = name
+
+        if breed not in self.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return
+        self.breed = breed
